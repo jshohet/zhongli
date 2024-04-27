897630ef1215cc9e18c6e01f6e972eb2139ba7c3
jshohet <parykeeth90@gmail.com>
2024-04-27 15:33:20 -0400

    HAPPY BDAY

--- src/commands/misc/hi.js
+++ src/commands/misc/hi.js
@@ -1,13 +1,21 @@
-const hi = require('../../assets/zhongli-hi.json')
+const hi = require("../../assets/zhongli-hi.json");
 
-module.exports={
-    name: "hi",
-    description: "Say hi to Zhongli!",
+module.exports = {
+  name: "hi",
+  description: "Say hi to Zhongli!",
 
-    callback: (client, interaction) =>{
-
+  callback: async (client, interaction) => {
+    await interaction.deferReply();
     const random = Math.floor(Math.random() * hi.length);
 
-        interaction.reply(hi[random]);
+    const today = new Date();
+
+    if (today.getDate() === 11 && today.getMonth() === 5) {
+      await interaction.editReply(
+        "Such as the diamonds embedded within the mountains outlasting routine decay and erosion, your existence too, my dear friend, adds beauty and permanence to this transient world. On this auspicious day of your birth's anniversary, I wish you countless blessings equivalent to the unceasing grains of golden sand by the sea. Happy birthday to you."
+      );
+    } else {
+      await interaction.editReply(hi[random]);
     }
-}
+  },
+};
